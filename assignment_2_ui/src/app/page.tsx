a36f9ba36e0b06f4ae32e6db11e3fb63a64b4c9c
Natthapong <natthapongchuen12@gmail.com>
2025-12-19 17:56:33 +0700

    Update DashboardPage
    update background color and add some shadow

--- assignment_2_ui/src/app/page.tsx
+++ assignment_2_ui/src/app/page.tsx
@@ -44,8 +44,8 @@ export default function DashboardPage() {
   const transactionsFiltered = Filterchangelist();
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-[#ffd6e8]/30 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white shadow-2xl dark:bg-black sm:items-start">
         <SummaryCard 
           totalincome = {totalIncome}
           totalexpense = {totalExpense}
